259f15e1566fe14ba117f13bd13341f60e86b47d
Jeremi_aron <chancanjeremiaron@gmail.com>
2025-01-16 21:26:07 -0500

    feat: :sparkles: Add post controller and response api

---
+++ src/main/java/com/elegant/socialnetwork/response/ApiResponse.java
@@ -0,0 +1,42 @@
+package com.elegant.socialnetwork.response;
+
+public class ApiResponse {
+    private String message;
+    private boolean status;
+
+    public ApiResponse() {};
+
+    public ApiResponse(String message, boolean status) {
+        this.message = message;
+        this.status = status;
+    }
+
+    /**
+     * @return String return the message
+     */
+    public String getMessage() {
+        return message;
+    }
+
+    /**
+     * @param message the message to set
+     */
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    /**
+     * @return boolean return the status
+     */
+    public boolean isStatus() {
+        return status;
+    }
+
+    /**
+     * @param status the status to set
+     */
+    public void setStatus(boolean status) {
+        this.status = status;
+    }
+
+}
