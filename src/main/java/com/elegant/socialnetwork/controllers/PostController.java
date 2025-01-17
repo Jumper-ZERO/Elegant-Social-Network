f980cd80bb981c306d964664608c330d9a4a89f3
Jeremi_aron <chancanjeremiaron@gmail.com>
2025-01-16 22:05:38 -0500

    fix: :bug: Replace get mapping for post in ambiguous mapping error

--- src/main/java/com/elegant/socialnetwork/controllers/PostController.java
+++ src/main/java/com/elegant/socialnetwork/controllers/PostController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -43,7 +44,7 @@ public class PostController {
         return new ResponseEntity<Post>(post, HttpStatus.ACCEPTED);
     }
 
-    @PostMapping("/user/{userId}")
+    @GetMapping("/user/{userId}")
     public ResponseEntity<List<Post>> findUserPost(@PathVariable Integer userId) {
         List<Post> posts = postService.findPostByUserId(userId);
         return new ResponseEntity<List<Post>>(posts, HttpStatus.ACCEPTED);
