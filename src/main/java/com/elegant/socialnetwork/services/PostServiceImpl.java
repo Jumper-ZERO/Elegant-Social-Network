d9e45478c51bfbbb40c503503915c235ee4de7d6
Jeremi_aron <chancanjeremiaron@gmail.com>
2025-01-16 22:02:32 -0500

    fix: :bug: Add service annotation

--- src/main/java/com/elegant/socialnetwork/services/PostServiceImpl.java
+++ src/main/java/com/elegant/socialnetwork/services/PostServiceImpl.java
@@ -4,12 +4,14 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.elegant.socialnetwork.models.Post;
 import com.elegant.socialnetwork.models.User;
 import com.elegant.socialnetwork.repository.PostRepository;
 import com.elegant.socialnetwork.repository.UserRepository;
 
+@Service
 public class PostServiceImpl implements PostService {
 
     @Autowired
