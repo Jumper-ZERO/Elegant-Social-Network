d56c0d1191cbd98137403936db3948b8f657fa93
Jeremi_aron <chancanjeremiaron@gmail.com>
2025-01-27 20:00:24 +0000

    build: :heavy_plus_sign: Add jwt dependencies

--- src/main/java/com/elegant/socialnetwork/config/AppConfig.java
+++ src/main/java/com/elegant/socialnetwork/config/AppConfig.java
@@ -4,20 +4,19 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
-
-import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 @EnableWebSecurity
 public class AppConfig {
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests(Authorize -> Authorize.requestMatchers("/api/**")
-                .authenticated()
-                .anyRequest()
-                .permitAll())
-                .httpBasic(withDefaults())
+        http.sessionManagement(managent -> managent.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(Authorize -> Authorize.requestMatchers("/api/**")
+                        .authenticated()
+                        .anyRequest()
+                        .permitAll())
                 .csrf(csrf -> csrf.disable());
         return http.build();
     }
