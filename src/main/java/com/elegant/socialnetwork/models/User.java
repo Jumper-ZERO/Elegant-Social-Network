8f3af5d38d5da6f80129337cedf490341fb60b57
Jeremi_aron <chancanjeremiaron@gmail.com>
2025-01-27 19:57:21 -0500

    fix: :bug: Add generating value in id

--- src/main/java/com/elegant/socialnetwork/models/User.java
+++ src/main/java/com/elegant/socialnetwork/models/User.java
@@ -6,6 +6,8 @@ import java.util.List;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
@@ -15,6 +17,7 @@ import jakarta.persistence.Table;
 public class User {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String firstName;
     private String lastName;
